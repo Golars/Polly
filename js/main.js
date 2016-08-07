10f13391b47ad7686967eaa473013137ffc492b6
Golars <titorens@gmail.com>
2016-08-07 03:01:54 +0300

    +add first version

---
+++ js/main.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+    console.log(123);
+    var $elems = $('.slider .el')
+    var $active = $('.slider .el.active')
+    if(!$active.length) $elems.eq(0).addClass('active');
+
+    $elems.eq(($elems.index($active) + 1) % $elems.length).addClass('active');
+    $active.removeClass('active');
+}, 12000)
