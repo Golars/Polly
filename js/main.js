46606faf40d32ad8cfc7c01bec120026b18b9319
Golars <titorens@gmail.com>
2016-08-13 13:39:43 +0300

    +fix js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,14 @@ setInterval(function(){
     $active.removeClass('active');
 }, 12000)
 
-$(document).ready(function () {
+$(document).ready(function(){
 
-    $('#selector').pongstgrm({
+    $('#recent').pongstgrm({
         accessId:     '35c6605ef49f44cea200ac967c8f2add',
-        accessToken:  '781299033df94a458dec1f3e691b9ae0'
+        accessToken:  '781299033df94a458dec1f3e691b9ae0',
+        show: 'recent', // 'recent', 'feed', 'liked', 'user'
+        count : 8, // 1(min) - 40(max), instagram limits the maximum number of photos to 40
+        pager : true // true or false (enables/disable load more button)
     });
 
-    $('#instagram').pongstgrm({ show: 'recent' });
 });
-
