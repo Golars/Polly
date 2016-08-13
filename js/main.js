d7a61cd74a8653a0b8f6031a67e7fad0f4c0c7e2
Golars <titorens@gmail.com>
2016-08-13 14:31:39 +0300

    +add pictures

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ setInterval(function(){
     $elems.eq(($elems.index($active) + 1) % $elems.length).addClass('active');
     $active.removeClass('active');
 }, 12000)
+
+var masonry;
+imagesLoaded($('body'), function() {
+    masonry = new Masonry($('#pictures')[0], {
+        itemSelector: '.el',
+        animate: true,
+        percentPosition: true,
+        isFitWidth: true,
+        stamp: '.stamp'
+    })
+});
